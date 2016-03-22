950673ef0fd2fe3cd5cf8c67d36d37b0703757b7
Ryan Butler <butlerrc30@gmail.com>
2016-03-22 15:56:07 -0500

    added profile clean

---
+++ Profiles/SoYouAddedEqualtoGPO.ps1
@@ -0,0 +1,60 @@
+﻿#Clears all UPM default exclusion path contents
+#Maybe you forgot to remove the = from the UPM GPO path exclusions?
+#Ryan Butler 3-22-16
+ 
+CLS
+#Default excludes https://docs.citrix.com/en-us/profile-management/5/upm-tuning-den/upm-include-exclude-defaults-den.html along with some google
+$excludes = "AppData\LocalLow\",`
+"AppData\Local\Microsoft\Windows\Temporary Internet Files\",`
+"AppData\Local\Microsoft\Windows\Burn\",`
+"AppData\Local\Microsoft\Windows\CD Burning\",`
+"AppData\Local\Microsoft\Windows Live\",`
+"AppData\Local\Microsoft\Windows Live Contacts\",`
+"AppData\Local\Microsoft\Terminal Server Client\",`
+"AppData\Local\Microsoft\Messenger\",`
+"AppData\Local\Microsoft\OneNote\",`
+"AppData\Local\Microsoft\Outlook\",`
+"AppData\Local\Windows Live\",`
+"AppData\Local\Sun\",`
+"AppData\Local\Temp\",`
+"AppData\Local\Google\Chrome\User Data\Default\Media Cache\",`
+"AppData\Local\Google\Chrome\User Data\Default\JumpListIcons\",`
+"AppData\Local\Google\Chrome\User Data\Default\JumpListIconsOld\",`
+"AppData\Roaming\Sun\Java\Deployment\cache\",`
+"AppData\Roaming\Sun\Java\Deployment\log\",`
+"AppData\Roaming\Sun\Java\Deployment\tmp\",`
+"AppData\Local\Google\Chrome\User Data\Default\Cache\",`
+"AppData\Local\Google\Chrome\User Data\Default\Cached Theme Images\"
+
+
+#profile store.  Please note trailing \
+$profpath =  "D:\User Data\ctxprofiles\"
+
+
+##################Script
+
+#Gets all profile folders
+$profiles = Get-ChildItem -Path $profpath|where{$_.psiscontainer -eq $true}
+
+foreach ($profile in $profiles)
+{
+    
+    #checks each folder and removes contents if found
+    foreach ($exclude in $excludes)
+    {
+
+    $checkpath = $profile.FullName + "\UPM_Profile\" + $exclude
+
+    write-host $checkpath
+
+        if (test-path $checkpath)
+        {
+            write-host "Found path and removing contents" -ForegroundColor Green
+            remove-item -Path ($checkpath + "*") -Recurse -Force
+        }
+        else
+        {
+            write-host "Path not found" -ForegroundColor Yellow
+        }
+    }
+}
