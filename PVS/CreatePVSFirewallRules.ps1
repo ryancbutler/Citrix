00ce725462de44e67f78b98c6af6f3e3d5958dd5
Ryan Butler <butlerrc30@gmail.com>
2017-10-11 10:37:20 -0500

    PVS firewall rules

---
+++ PVS/CreatePVSFirewallRules.ps1
@@ -0,0 +1,18 @@
+#Creates firewall rules for PVS
+#Run as admin
+#http://www.carlstalhood.com/netscaler-firewall-rules/#pvs
+
+#TFTP
+New-NetFirewallRule -DisplayName "Citrix PVS TFTP" -Name "PVSTFTP" -Group "Citrix Provisioning Services" -Direction Inbound –LocalPort 69 -Protocol UDP -Action Allow
+
+#PXE
+New-NetFirewallRule -DisplayName "Citrix PVS PXE" -Name "PVSPXE" -Group "Citrix Provisioning Services" -Direction Inbound –LocalPort (67,4011) -Protocol UDP -Action Allow
+
+#Streaming
+New-NetFirewallRule -DisplayName "Citrix PVS Streaming" -name "PVSSTREAMING" -Group "Citrix Provisioning Services" -Direction Inbound –LocalPort 6910-6930 -Protocol UDP -Action Allow
+
+#BDM
+New-NetFirewallRule -DisplayName "Citrix PVS BDM" -name "PVSBDM" -Group "Citrix Provisioning Services" -Direction Inbound –LocalPort 6969,2071 -Protocol UDP -Action Allow
+
+#Imaging Wizard to SOAP
+New-NetFirewallRule -DisplayName "Citrix PVS SOAP" -name "PVSSOAP" -Group "Citrix Provisioning Services" -Direction Inbound –LocalPort 54321,54322,54323 -Protocol TCP -Action Allow
