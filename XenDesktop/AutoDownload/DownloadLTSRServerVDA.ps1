550c8e62ee5970a815a3741b8d84a5e1ede9c9a4
Ryan Butler <ryan@techdrabble.com>
2019-07-22 15:36:53 -0500

    ltsr

---
+++ XenDesktop/AutoDownload/DownloadLTSRServerVDA.ps1
@@ -0,0 +1,41 @@
+﻿#Downloads Citrix CVAD LTSR 7.15 CU 4 Server VDA
+#Can be used as part of a pipeline or MDT task sequence.
+#Ryan Butler TechDrabble.com @ryan_c_butler 07/19/2019
+
+#Uncomment to use plain text or env variables
+#$CitrixUserName = $env:citrixusername
+#$CitrixPassword = $env:citrixpassword
+
+#Uncomment to use credential object
+$creds = get-credential
+$CitrixUserName = $creds.UserName
+$CitrixPassword = $creds.GetNetworkCredential().Password
+
+$downloadpath = "C:\temp\VDAServerSetup_7_15_4000.exe"
+
+#Initialize Session 
+$R=Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response" -SessionVariable websession
+
+#Set Form
+$form = $R.Forms[0]
+$form.Fields["persistent"]="on"
+$form.Fields["userName"]= $CitrixUserName
+$form.Fields["password"]= $CitrixPassword
+
+#Authenticate
+$R=Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response") -WebSession $websession -Method POST -Body $form.Fields -contentType "application/x-www-form-urlencoded" -UseBasicParsing
+
+
+$download = Invoke-WebRequest -Uri ('https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=15440&URL=https://downloads.citrix.com/15440/VDAServerSetup_7_15_4000.exe') -WebSession $websession -UseBasicParsing -MaximumRedirection 100 -Verbose -Method GET
+$webform = @{
+"chkAccept" = "on"
+"__EVENTTARGET" = "clbAccept_0"
+"__EVENTARGUMENT" = "clbAccept_0_Click"
+"__VIEWSTATE" = ($download.InputFields|Where-Object{$_.id -eq "__VIEWSTATE"}).value
+"__EVENTVALIDATION" = ($download.InputFields|Where-Object{$_.id -eq "__EVENTVALIDATION"}).value
+}
+
+#Download
+Invoke-WebRequest -Uri ("https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=15440&URL=https%3a%2f%2fdownloads.citrix.com%2f15440%2fVDAServerSetup_7_15_4000.exe") -WebSession $websession -Method POST -Body $webform -contentType "application/x-www-form-urlencoded" -UseBasicParsing -OutFile $downloadpath -Verbose
+#Invoke-WebRequest -Uri ("https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=16110&URL=https%3a%2f%2fdownloads.citrix.com%2f16110%2fVDAServerSetup_1906.exe") -WebSession $websession -Method POST -Body $webform -contentType "application/x-www-form-urlencoded" -UseBasicParsing -OutFile $downloadpath -Verbose
+
--- XenDesktop/AutoDownload/README.md
+++ XenDesktop/AutoDownload/README.md
@@ -1,9 +1,14 @@
 # Auto Download Scripts
 PowerShell Scripts that download Citrix media used in a pipeline, Packer job, task sequence, etc.  
-**NOTE: By default script(s) prompts for credenitals but can be changed. If needed please take caution in storing password! Not recommened to store in plaintext!**
+
+**NOTE: By default script(s) prompts for credenitals but can be changed. If needed, please take caution in storing password! Not recommened to store in plaintext!**
+
+**NOTE: There are currently no error handling if credentials are mistyped!**
 
 ## Currently Works with
 - Citrix Optimizer https://support.citrix.com/article/CTX224676
   - 7-zip is required for the extract process
 - Citrix CVAD Server VDA 1906
 - Citrix CVAD Desktop VDA 1906
+- Citrix CVAD Server VDA 7.15 CU 4 (LTSR)
+- Citrix CVAD Desktop VDA 7.15 CU 4 (LTSR)
