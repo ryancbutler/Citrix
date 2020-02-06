a9b3f6b82db8593089a377584343f5d8e9bf45ec
Ryan Butler <ryancbutler@users.noreply.github.com>
2020-02-06 10:12:06 -0600

    Adding better auto download feature (#17)
    * new function
    
    * docs
    
    * cleaner
    
    * adding image

---
+++ XenDesktop/AutoDownload/GetVDAS.ps1
@@ -0,0 +1,41 @@
+#Example script to download multiple VDA and ISO versions from Citrix.com
+#Ryan Butler 2/6/2020
+
+#Folder dialog
+#https://stackoverflow.com/questions/25690038/how-do-i-properly-use-the-folderbrowserdialog-in-powershell
+Function Get-Folder($initialDirectory)
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return ($folder + "\") 
+}
+
+#Prompt for folder path
+$path = Get-Folder
+
+#Import Download Function
+. ./Helpers/Get-CTXBinary.ps1
+
+$creds = Get-Credential -Message "Citrix Credentials"
+$CitrixUserName = $creds.UserName
+$CitrixPassword = $creds.GetNetworkCredential().Password
+
+#Imports CSV with download information
+$downloads = import-csv -Path ".\Helpers\Downloads.csv" -Delimiter ","
+
+#Use CTRL to select multiple
+$dls = $downloads | Out-GridView -PassThru -Title "Select VDA or ISO to download.  CTRL to select multiple"
+
+#Processes each download
+foreach ($dl in $dls) {
+    Get-CTXBinary -DLNUMBER $dl.dlnumber -DLEXE $dl.filename -CitrixUserName $CitrixUserName -CitrixPassword $CitrixPassword -DLPATH $path
+}
---
+++ XenDesktop/AutoDownload/Helpers/Get-CTXBinary.ps1
@@ -0,0 +1,55 @@
+﻿function get-ctxbinary {
+	<#
+.SYNOPSIS
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication
+.DESCRIPTION
+  Downloads a Citrix VDA or ISO from Citrix.com utilizing authentication.
+  Ryan Butler 2/6/2020
+.PARAMETER DLNUMBER
+  Number assigned to binary download
+.PARAMETER DLEXE
+  File to be downloaded
+.PARAMETER DLPATH
+  Path to store downloaded file. Must contain following slash (c:\temp\)
+.PARAMETER CitrixUserName
+  Citrix.com username
+.PARAMETER CitrixPassword
+  Citrix.com password
+.EXAMPLE
+  Get-CTXBinary -DLNUMBER "16834" -DLEXE "Citrix_Virtual_Apps_and_Desktops_7_1912.iso" -CitrixUserName "mycitrixusername" -CitrixPassword "mycitrixpassword" -DLPATH "C:\temp\"
+#>
+	Param(
+		[Parameter(Mandatory = $true)]$DLNUMBER,
+		[Parameter(Mandatory = $true)]$DLEXE,
+		[Parameter(Mandatory = $true)]$DLPATH,
+		[Parameter(Mandatory = $true)]$CitrixUserName,
+		[Parameter(Mandatory = $true)]$CitrixPassword
+	)
+	#Initialize Session 
+	Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response" -SessionVariable websession -UseBasicParsing | Out-Null
+
+	#Set Form
+	$form = @{
+		"persistent" = "on"
+		"userName"   = $CitrixUserName
+		"password"   = $CitrixPassword
+	}
+
+	#Authenticate
+	Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In?ReturnUrl=%2fUtility%2fSTS%2fsaml20%2fpost-binding-response") -WebSession $websession -Method POST -Body $form -ContentType "application/x-www-form-urlencoded" -UseBasicParsing|Out-Null
+	$dlurl = "https://secureportal.citrix.com/Licensing/Downloads/UnrestrictedDL.aspx?DLID=${DLNUMBER}&URL=https://downloads.citrix.com/${DLNUMBER}/${DLEXE}"
+	$download = Invoke-WebRequest -Uri $dlurl -WebSession $websession -UseBasicParsing -Method GET
+	$webform = @{ 
+		"chkAccept"         = "on"
+		"__EVENTTARGET"     = "clbAccept_0"
+		"__EVENTARGUMENT"   = "clbAccept_0_Click"
+		"__VIEWSTATE"       = ($download.InputFields | Where-Object { $_.id -eq "__VIEWSTATE" }).value
+		"__EVENTVALIDATION" = ($download.InputFields | Where-Object { $_.id -eq "__EVENTVALIDATION" }).value
+	}
+
+	$outfile = ($DLPATH + $DLEXE)
+	#Download
+	Invoke-WebRequest -Uri $dlurl -WebSession $websession -Method POST -Body $webform -ContentType "application/x-www-form-urlencoded" -UseBasicParsing -OutFile $outfile
+	return $outfile
+}
+
